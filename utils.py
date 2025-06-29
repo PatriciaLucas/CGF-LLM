3a2707545076c93b4a8db2503d0abbd24fb8cb2e
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-06-29 17:58:32 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,6 +22,19 @@ def get_dataset(name):
         return "There is no dataset with that name."
 
 
+def executemany(sql, forecasts, database_path):
+    """
+    Função para executar INSERT INTO
+    :parametro sql: string com código sql
+    :parametro database_path: caminho para o banco de dados
+    :return: dataframe com os valores retornados pela consulta sql
+    """
+    with contextlib.closing(sqlite3.connect(database_path)) as conn: # auto-closes
+        with conn: # auto-commits
+            with contextlib.closing(conn.cursor()) as cursor: # auto-closes
+                cursor.executemany(sql, forecasts)
+                return cursor.fetchall()
+
 def execute_insert(sql,data,database_path):
     """
     Função para executar INSERT INTO
